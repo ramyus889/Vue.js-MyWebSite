de5b2d76d0f0bd91d6e64b9eec12077678b4ceff
ramyus889 <ramyus889.com>
2024-10-08 19:28:05 +0300

    add

--- src/components/UseGridCard.vue
+++ src/components/UseGridCard.vue
@@ -1,122 +1,119 @@
-<script setup></script>
+<script setup>
+const items1 = [
+  {
+    id: 1,
+    image: '/Screenshot_1.png',
+    link: 'https://web-illa-cloud.netlify.app',
+    description: 'web-illa-cloud.netlify.app'
+  },
+  {
+    id: 2,
+    image: '/Screenshot_2.png',
+    link: 'https://magedeveloper.netlify.app',
+    description: 'magedeveloper.netlify.app'
+  },
+  {
+    id: 3,
+    image: '/Screenshot_3.png',
+    link: 'https://fastgendev.netlify.app',
+    description: 'fastgendev.netlify.app'
+  },
+  {
+    id: 4,
+    image: '/Screenshot_4.png',
+    link: 'https://vuesneakers747.netlify.app',
+    description: 'vuesneakers747.netlify.app'
+  },
+  {
+    id: 5,
+    image: '/Screenshot_5.png',
+    link: 'https://hyperwebdev.netlify.app',
+    description: 'hyperwebdev.netlify.app'
+  }
+];
+const items2 = [
+  {
+    id: 1,
+    image: '/Screenshot_6.png',
+    link: 'https://31-saa-s.vercel.app/',
+    description: '31-saa-s.vercel.app'
+  },
+  {
+    id: 2,
+    image: '/Screenshot_7.png',
+    link: 'https://mirror-world-seven.vercel.app/',
+    description: 'mirror-world-seven.vercel.app'
+  },
+  {
+    id: 3,
+    image: '/Screenshot_8.png',
+    link: 'https://next-js-pizza-sigma.vercel.app/',
+    description: 'next-js-pizza-sigma.vercel.app'
+  },
+
+  {
+    id: 4,
+    image: '/Screenshot_9.png',
+    link: 'https://lesson-english2-two.vercel.app/',
+    description: 'lesson-english2-two.vercel.app'
+  },
+  {
+    id: 5,
+    image: '/Screenshot_10.png',
+    link: 'https://personalwebfrontend.vercel.app/',
+    description: 'personalwebfrontend.vercel.app'
+  }
+];
+</script>
 <template>
   <div class="border border-slate-600 mt-2"></div>
   <div class="pt-4 pb-3 px-5 text-3xl font-bold">Vue.js (Netlify)</div>
   <div class="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 xl:grid-cols-4 mt-5">
     <div
-      class="md:max-w-[630px] w-full border border-slate-600 p-[15px] items-center flex justify-center"
+      v-for="item in items1"
+      :key="item.id"
+      class="md:max-w-[630px] relative w-full border border-slate-600 p-[15px] flex flex-col gap-4 justify-center"
     >
-      <a href="https://vuesneakers747.netlify.app" target="_blank">
-        <img
-          src="/Screenshot_4.png"
-          class="hover:p-2 cursor-pointer rounded-xl transition-all duration-500 ease-in-out"
-          alt=""
-        />
-      </a>
-    </div>
-    <div
-      class="md:max-w-[630px] w-full border border-slate-600 p-[15px] items-center flex justify-center"
-    >
-      <a href="https://magedeveloper.netlify.app" target="_blank">
-        <img
-          src="/Screenshot_2.png"
-          alt=""
-          class="hover:p-2 cursor-pointer rounded-xl transition-all duration-500 ease-in-out"
-        />
-      </a>
-    </div>
-
-    <div
-      class="md:max-w-[630px] w-full border border-slate-600 p-[15px] items-center flex justify-center"
-    >
-      <a href="https://web-illa-cloud.netlify.app" target="_blank">
-        <img
-          src="/Screenshot_1.png"
-          alt=""
-          class="hover:p-2 cursor-pointer border-slate-600 rounded-xl transition-all duration-500 ease-in-out"
-        />
-      </a>
-    </div>
-    <div
-      class="md:max-w-[630px] w-full border border-slate-600 p-[15px] items-center flex justify-center"
-    >
-      <a href="https://fastgendev.netlify.app" target="_blank">
-        <img
-          src="/Screenshot_3.png"
-          alt=""
-          class="hover:p-2 cursor-pointer rounded-xl transition-all duration-500 ease-in-out"
-        />
-      </a>
-    </div>
-    <div
-      class="md:max-w-[630px] w-full border border-slate-600 p-[15px] items-center flex justify-center"
-    >
-      <a href="https://hyperwebdev.netlify.app" target="_blank">
-        <img
-          src="/Screenshot_5.png"
-          alt=""
-          class="hover:p-2 cursor-pointer rounded-xl transition-all duration-500 ease-in-out"
-        />
-      </a>
+      <div class="flex justify-center items-center">
+        <a :href="item.link" target="_blank">
+          <img
+            :src="item.image"
+            class="hover:translate-y-[-5px] cursor-pointer rounded-xl transition-all duration-500 ease-in-out mb-12"
+            alt=""
+          />
+        </a>
+      </div>
+      <div class="absolute bottom-0 left-0 right-0">
+        <div class="flex justify-between items-center gap-10 px-3 py-6 text-[14px]">
+          <div class="text-start">{{ item.description }}</div>
+          <a href="#" class="hover:underline">Перейти →</a>
+        </div>
+      </div>
     </div>
   </div>
   <div class="border border-slate-600 mt-[100px]"></div>
   <div class="pt-4 pb-3 px-5 text-3xl font-bold">Next.js (Vercel)</div>
   <div class="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 xl:grid-cols-4 mt-5">
     <div
-      class="md:max-w-[630px] w-full border border-slate-600 p-[15px] items-center flex justify-center"
+      v-for="item in items2"
+      :key="item.id"
+      class="md:max-w-[630px] w-full relative border border-slate-600 p-[15px] flex flex-col gap-4 justify-center"
     >
-      <a href="https://31-saa-s.vercel.app/" target="_blank">
-        <img
-          src="/Screenshot_6.png"
-          class="hover:p-2 cursor-pointer rounded-xl transition-all duration-500 ease-in-out"
-          alt=""
-        />
-      </a>
-    </div>
-    <div
-      class="md:max-w-[630px] w-full border border-slate-600 p-[15px] items-center flex justify-center"
-    >
-      <a href="https://mirror-world-seven.vercel.app/" target="_blank">
-        <img
-          src="/Screenshot_7.png"
-          alt=""
-          class="hover:p-2 cursor-pointer rounded-xl transition-all duration-500 ease-in-out"
-        />
-      </a>
-    </div>
-    <div
-      class="md:max-w-[630px] w-full border border-slate-600 p-[15px] items-center flex justify-center"
-    >
-      <a href="https://next-js-pizza-sigma.vercel.app/" target="_blank">
-        <img
-          src="/Screenshot_8.png"
-          alt=""
-          class="hover:p-2 cursor-pointer rounded-xl transition-all duration-500 ease-in-out"
-        />
-      </a>
-    </div>
-    <div
-      class="md:max-w-[630px] w-full border border-slate-600 p-[15px] items-center flex justify-center"
-    >
-      <a href="https://lesson-english2-two.vercel.app/" target="_blank">
-        <img
-          src="/Screenshot_9.png"
-          alt=""
-          class="hover:p-2 cursor-pointer rounded-xl transition-all duration-500 ease-in-out"
-        />
-      </a>
-    </div>
-    <div
-      class="md:max-w-[630px] w-full border border-slate-600 p-[15px] items-center flex justify-center"
-    >
-      <a href="https://personalwebfrontend.vercel.app/" target="_blank">
-        <img
-          src="/Screenshot_10.png"
-          alt=""
-          class="hover:p-2 cursor-pointer rounded-xl transition-all duration-500 ease-in-out"
-        />
-      </a>
+      <div class="flex justify-center items-center">
+        <a :href="item.link" target="_blank">
+          <img
+            :src="item.image"
+            class="hover:translate-y-[-5px] cursor-pointer rounded-xl transition-all duration-500 ease-in-out mb-12"
+            alt=""
+          />
+        </a>
+      </div>
+      <div class="absolute bottom-0 left-0 right-0">
+        <div class="flex justify-between items-center gap-10 px-3 py-6 text-[14px]">
+          <div class="text-start">{{ item.description }}</div>
+          <a href="#" class="hover:underline">Перейти →</a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
