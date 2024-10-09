8d64f94d921675e49a2f9dc97fead834eaff720a
ramyus889 <ramyus889.com>
2024-10-09 14:16:45 +0300

    add

--- src/components/UseFooter.vue
+++ src/components/UseFooter.vue
@@ -4,7 +4,11 @@
       <img src="/img/Code/frontend.jpg" alt="" class="w-[45px] rounded-full" />
     </RouterLink>
     <div class="flex gap-5">
-      <a href="https://hh.ru/profile/me?hhtmFrom=ProfileActivator" target="_blank" class="">
+      <a
+        href="https://hh.ru/applicant/resumes/view?resume=08775528ff0dd8bcdd0039ed1f455342777341"
+        target="_blank"
+        class=""
+      >
         <img src="/hhLogo.png" alt="" class="w-[45px]" />
       </a>
       <a href="https://github.com/ramyus889" target="_blank" class="">
