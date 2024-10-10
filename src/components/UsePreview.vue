6a6a54bd338aa33fd36edd8aaf50e8ece1d97919
ramyus889 <ramyus889.com>
2024-10-10 10:34:19 +0300

    add

--- src/components/UsePreview.vue
+++ src/components/UsePreview.vue
@@ -44,7 +44,7 @@ defineProps({
 <template>
   <div
     data-aos="fade-up"
-    data-aos-delay="100"
+    data-aos-delay="300"
     class="md:max-w-[630px] relative w-full border border-slate-600 p-[15px] flex flex-col gap-4 justify-center"
   >
     <div class="flex justify-center items-center">
