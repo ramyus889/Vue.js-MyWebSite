373ec382a5e40c93f4088c9ed0ec3d68f010e1db
ramyus889 <ramyus889.com>
2024-11-03 12:55:35 +0300

    обновил router-link теперь ссылки буду записывать используя routeUrl, имена ссылок будут использоваться по массивам

--- src/components/UseHeader.vue
+++ src/components/UseHeader.vue
@@ -1,22 +1,23 @@
 <script setup>
+import { routeUrl } from '@/router/routes';
 import UseDrawer from './UseDrawer.vue';
 </script>
 <template>
   <div class="fixed top-0 left-0 right-0 z-50 bg-[#181818] backdrop-blur-[50px] bg-opacity-[0.5]">
-    <header class="flex justify-between items-center px-5 py-2 border-b border-slate-600">
-      <RouterLink to="/" class="flex gap-3 items-center">
+    <header class="flex items-center justify-between px-5 py-2 border-b border-slate-600">
+      <RouterLink to="/" class="flex items-center gap-3">
         <img src="/img/Code/frontend.jpg" alt="" class="w-[60px] rounded-full" />
         <div class="*:font-bold leading-[23px] text-xl">
           <div class="">PortfolioDev</div>
         </div>
       </RouterLink>
-      <div class="sm:hidden flex">
+      <div class="flex sm:hidden">
         <UseDrawer />
       </div>
-      <div class="sm:flex hidden gap-5 pe-5">
-        <RouterLink to="/Profile" class="">Profile</RouterLink>
-        <RouterLink to="/">Work</RouterLink>
-        <RouterLink to="/About" class="">Skills</RouterLink>
+      <div class="hidden gap-5 sm:flex pe-5">
+        <RouterLink :to="`${routeUrl[2].path}`" class="">Profile</RouterLink>
+        <RouterLink :to="`${routeUrl[0].path}`">Work</RouterLink>
+        <RouterLink :to="`${routeUrl[1].path}`" class="">Skills</RouterLink>
       </div>
     </header>
   </div>
--- src/router/routes.js
+++ src/router/routes.js
@@ -2,17 +2,29 @@ import UseHome from '@/components/UseHome.vue';
 import UseAbout from '@/Pages/About/UseAbout.vue';
 import UseProfile from '@/Pages/Profile/UseProfile.vue';
 
+export const routeUrl = [
+  {
+    path: '/'
+  },
+  {
+    path: '/About'
+  },
+  {
+    path: '/Profile'
+  }
+];
+
 export const routes = [
   {
-    path: '/',
+    path: `${routeUrl[0].path}`,
     component: UseHome
   },
   {
-    path: '/About',
+    path: `${routeUrl[1].path}`,
     component: UseAbout
   },
   {
-    path: '/Profile',
+    path: `${routeUrl[2].path}`,
     component: UseProfile
   }
 ];
