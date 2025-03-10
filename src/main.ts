e0efddc03968def2d1d85ae3af1ace38c86fb51f
ramyus889 <ramyus889.com>
2025-03-10 13:48:36 +0300

    добавлено поведение прокрутки для маршрутизатора: восстановление сохраненной позиции или возврат к верхней части страницы

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,14 @@ app.use(PrimeVue, {
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0 };
+    }
+  }
 });
 
 app.use(router);
